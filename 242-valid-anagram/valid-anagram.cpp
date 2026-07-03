efe9d480a00dcfa4eb3554e9ddbcf248b40115f7
Mustafa-9897 <mustafababude02@gmail.com>
2026-07-04 00:25:43 +0530

    Time: 2 ms (51.04%) | Memory: 9.7 MB (49.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // if(s.size()!=t.size()){
+        //     return false;
+        // }
+        // unordered_map<char,int>mpp1;
+        // unordered_map<char,int>mpp2;
+        // for(int i=0;i<s.size();i++){
+        //     mpp1[s[i]]++;
+        // }
+        // for(int i=0;i<t.size();i++){
+        //     mpp2[t[i]]++;           
+        // }
+        // return mpp1==mpp2;
+
+        //    SLIGHT BETTER
         if(s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int>mpp1;
-        unordered_map<char,int>mpp2;
-        for(int i=0;i<s.size();i++){
-            mpp1[s[i]]++;
+        unordered_map<char,int>mpp;
+        for(auto ch:s) mpp[ch]++;
+        for(auto ch:t) mpp[ch]--;
+        for(auto it:mpp){
+            if(it.second!=0){
+                return false;
+            }
         }
-        for(int i=0;i<t.size();i++){
-            mpp2[t[i]]++;           
-        }
-        return mpp1==mpp2;
+        return true;
     }
 };
