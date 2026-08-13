d8d00dd18153ac6514db8964a84d62f94beb261b
Mustafa-9897 <mustafababude02@gmail.com>
2026-08-13 23:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.28%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int cnt=1;
+        while(n!=1){
+            if(n%2==1) cnt += 1;
+            n /= 2;
+        }
+        return cnt;
+    }
+};
