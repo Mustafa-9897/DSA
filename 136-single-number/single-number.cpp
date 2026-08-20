231cda653dbd93f5de4e5bf85b32048c6be640bf
Mustafa-9897 <mustafababude02@gmail.com>
2026-08-20 23:54:40 +0530

    Time: 7 ms (18.13%) | Memory: 24 MB (7.47%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //  BRUTE
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it : mpp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return 0;
+        // int ans=0;
+        // for(int i=0;i<nums.size();i++){
+        //     ans=ans^nums[i];
+        // }
+        // return ans;      
+    }
+};
