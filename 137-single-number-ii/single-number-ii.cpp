0aea157a12e6ffa7b610ffa55561e7acb37f7bc6
Mustafa-9897 <mustafababude02@gmail.com>
2026-08-20 23:59:35 +0530

    Time: 2 ms (38.64%) | Memory: 13.4 MB (35.16%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,15 +2,31 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         //   BRUTE
-        unordered_map<int,int>mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if(it.second==1){
-                return it.first;
+        // unordered_map<int,int>mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second==1){
+        //         return it.first;
+        //     }
+        // }
+        // return 0;
+
+
+        // BETTER
+        int ans=0;
+        for(int bitidx=0;bitidx<=31;bitidx++){
+            int cnt=0;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]&(1<<bitidx)){
+                    cnt++;
+                }
+            }
+            if(cnt%3==1){
+                ans = ans | (1<<bitidx);
             }
         }
-        return 0;
+        return ans;
     }
 };
