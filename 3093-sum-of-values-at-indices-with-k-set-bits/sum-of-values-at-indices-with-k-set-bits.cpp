0cad243ebff1f7f937886677fa8388f45e875ec4
Mustafa-9897 <mustafababude02@gmail.com>
2026-08-23 00:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (83.45%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int helper(int num){
-        int cnt=0;
-        while(num!=0){
-            num = num & (num-1);
-            cnt++;
-        }
-        return cnt;
-        // int cnt=1;
-        // while(num!=1){
-        //     if(num%2==1) cnt++;
-        //     num /= 2;
+        // int cnt=0;
+        // while(num!=0){
+        //     num = num & (num-1);
+        //     cnt++;
         // }
         // return cnt;
+        int cnt=1;
+        while(num!=1){
+            if(num%2==1) cnt++;
+            num /= 2;
+        }
+        return cnt;
     }
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            int setbits=helper(i);
-            if(setbits==k){
+        // int sum=0;
+        // for(int i=0;i<nums.size();i++){
+        //     int setbits=helper(i);
+        //     if(setbits==k){
+        //         sum += nums[i];
+        //     }
+        // }
+        // return sum;
+
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (__builtin_popcount(i) == k) {
                 sum += nums[i];
             }
         }
