11dece28e96b34f67fdf7795a2529c44fcf51358
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-04 17:26:04 +0530

    Time: 3 ms (69.67%) | Memory: 16.7 MB (90.91%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>>ans;
+        unordered_map<int,vector<int>>mpp;
+        for(int i=0;i<groupSizes.size();i++){
+            int size=groupSizes[i];
+            mpp[size].push_back(i);
+            if(mpp[size].size()==size){
+                ans.push_back(mpp[size]);
+                mpp[size].clear();
+            }
+        }
+        return ans;
+    }
+};
