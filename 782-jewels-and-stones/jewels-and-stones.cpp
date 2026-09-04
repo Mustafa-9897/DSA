51f1df25cd4f424d7c4f07e248259c48fdc031c8
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-04 23:43:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.94%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_map<char,int>mpp;
+        for(int i=0;i<jewels.size();i++){
+            mpp[jewels[i]]=1;
+        }
+        int ans=0;
+        for(int i=0;i<stones.size();i++){
+            if(mpp.find(stones[i])!=mpp.end()){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
