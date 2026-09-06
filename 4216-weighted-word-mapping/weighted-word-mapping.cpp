579ba7988309915939483bcb0368fe0816fe2fa9
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-06 22:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (80.04%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans="";
+        for(int i=0;i<words.size();i++){
+            string s=words[i];
+            int sum=0;
+            for(int j=0;j<s.size();j++){
+                sum += weights[s[j]-'a'];
+            }
+            sum = sum % 26;
+            ans += char(122-sum);
+        }
+        return ans;
+    }
+};
