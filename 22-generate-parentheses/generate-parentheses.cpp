bc9c3f7c8cc9ea122596d174711c5b73dfc15da3
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-07 18:51:46 +0530

    Time: 3 ms (67.80%) | Memory: 15.6 MB (60.53%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void backtrack(string curr,int open,int close,int n,vector<string>&result){
+        if(curr.size()==2*n){
+            result.push_back(curr);
+            return ;
+        }
+        if(open<n){
+            backtrack(curr+'(',open+1,close,n,result);
+        }
+        if(close<open){
+            backtrack(curr+')',open,close+1,n,result);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>result;
+        backtrack("",0,0,n,result);
+        return result;
+    }
+};
