a8c3aad1618ed2a96fbba41086268ac322ec0e46
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-09 10:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (84.80%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -5,7 +5,7 @@ public:
         long long ans=0;
         for(long long i=1000;i<=n;){
             ans += n-i+1;
-            if(i>n/1000){
+            if(i>n/1000){ // we are no doing i=i*1000 in for loop itself because when after i=10^15 then before ending the for loop , we will do i=i*1000 then it will become 10^18 then it will exceed long long too ,that is why we are doing it inside for loop and letting it exceed 10^15.           
                 break;
             }
             i=i*1000;
