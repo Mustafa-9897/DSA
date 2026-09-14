5950b3e71740e66859c90af0166f59c7450f757f
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-14 23:13:58 +0530

    Time: 3 ms (61.12%) | Memory: 11.5 MB (73.80%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
-    void helper(int index,set<vector<int>>&st,vector<int>&curr,vector<bool>&used,vector<int>&nums){
+    // void helper(int index,set<vector<int>>&st,vector<int>&curr,vector<bool>&used,vector<int>&nums){
+    //     if(curr.size()==nums.size()){
+    //         st.insert(curr);
+    //         return ;
+    //     }
+    //     for(int i=0;i<nums.size();i++){
+    //         if(used[i]==false){
+    //             curr.push_back(nums[i]);        //  BRUTE
+    //             used[i]=true;
+    //             helper(i+1,st,curr,used,nums);
+    //             curr.pop_back();
+    //             used[i]=false;
+    //         }
+    //     }
+    // }
+    void helper(vector<vector<int>>&ans,vector<int>&curr,vector<bool>&used,vector<int>&nums){
         if(curr.size()==nums.size()){
-            st.insert(curr);
+            ans.push_back(curr);
             return ;
         }
         for(int i=0;i<nums.size();i++){
-            if(used[i]==false){
-                curr.push_back(nums[i]);
-                used[i]=true;
-                helper(i+1,st,curr,used,nums);
-                curr.pop_back();
-                used[i]=false;
-            }
+
+            if(used[i]==true) continue;
+            if(i>0 && nums[i]==nums[i-1] && used[i-1]==false) continue;
+            
+            used[i]=true;
+            curr.push_back(nums[i]);
+            helper(ans,curr,used,nums);
+            used[i]=false;
+            curr.pop_back();
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        // vector<vector<int>>ans;
+        // vector<int>curr;
+        // set<vector<int>>st;
+        // vector<bool>used(nums.size(),false);   // BRUTE
+        // helper(0,st,curr,used,nums);
+        // ans.assign(st.begin(),st.end());
+        // return ans;
+
+        //  OPTIMAL
         vector<vector<int>>ans;
         vector<int>curr;
-        set<vector<int>>st;
         vector<bool>used(nums.size(),false);
-        helper(0,st,curr,used,nums);
-        ans.assign(st.begin(),st.end());
+        sort(nums.begin(),nums.end());
+        helper(ans,curr,used,nums);
         return ans;
     }
 };
