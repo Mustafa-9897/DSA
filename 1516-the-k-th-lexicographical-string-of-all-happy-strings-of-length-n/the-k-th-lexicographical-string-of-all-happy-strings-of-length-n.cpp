9c251067f39e0f7326c958abbe5e5e108efd3a9b
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-15 12:27:07 +0530

    Time: 21 ms (22.16%) | Memory: 25.7 MB (14.01%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
-    string helper(string curr,int n,int &k){
+    // string helper(string curr,int n,int &k){
+    //     if(curr.size()==n){
+    //         k--;
+    //         if(k==0){
+    //             return curr;
+    //         }
+    //         return "";
+    //     }
+    //     if(curr.empty() || curr.back()!='a'){
+    //         string ans=helper(curr+'a',n,k);
+    //         if(!ans.empty()) return ans;
+    //     }
+    //     if(curr.empty() || curr.back()!='b'){
+    //         string ans=helper(curr+'b',n,k);
+    //         if(!ans.empty()) return ans;
+    //     }
+    //     if(curr.empty() || curr.back()!='c'){
+    //         string ans=helper(curr+'c',n,k);
+    //         if(!ans.empty()) return ans;
+    //     }
+    //     return "";
+    // }
+
+    // BRUTE
+    void helper(vector<string>&ans,string curr,int n){
         if(curr.size()==n){
-            k--;
-            if(k==0){
-                return curr;
-            }
-            return "";
+            ans.push_back(curr);
+            return ;
         }
         if(curr.empty() || curr.back()!='a'){
-            string ans=helper(curr+'a',n,k);
-            if(!ans.empty()) return ans;
+            helper(ans,curr+'a',n);
         }
         if(curr.empty() || curr.back()!='b'){
-            string ans=helper(curr+'b',n,k);
-            if(!ans.empty()) return ans;
+            helper(ans,curr+'b',n);
         }
         if(curr.empty() || curr.back()!='c'){
-            string ans=helper(curr+'c',n,k);
-            if(!ans.empty()) return ans;
+            helper(ans,curr+'c',n);
         }
-        return "";
     }
     string getHappyString(int n, int k) {
-        return helper("",n,k);       
+        //return helper("",n,k);  
+
+        // BRUTE
+        vector<string>ans;
+        helper(ans,"",n);
+        int cnt=1;
+        string ANS="";
+        for(int i=0;i<ans.size();i++){
+            if(cnt==k){
+                ANS=ans[i];
+                break;
+            }
+            cnt++;
+        }  
+        return ANS;   
     }
 };
