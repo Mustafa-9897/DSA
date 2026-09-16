a1bde4612f232c80f10e18128cced5163aa66951
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-16 18:18:18 +0530

    Time: 7 ms (83.77%) | Memory: 19.1 MB (59.69%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen=0;
-        int len;
-        for(int i=0;i<s.size();i++){
-            vector<int>hash(256,0);
-            for(int j=i;j<s.size();j++){
-                if(hash[s[j]]==1) break;
-                len = j-i+1;
-                hash[s[j]]=1;
-            }
-            maxlen=max(maxlen,len);
-        }
-        return maxlen;
+        //    BRUTE
+        // int maxlen=0;
+        // int len;
+        // for(int i=0;i<s.size();i++){
+        //     vector<int>hash(256,0);
+        //     for(int j=i;j<s.size();j++){
+        //         if(hash[s[j]]==1) break;
+        //         len = j-i+1;
+        //         hash[s[j]]=1;
+        //     }
+        //     maxlen=max(maxlen,len);
+        // }
+        // return maxlen;
 
-        //   OPTIMAL
+        //   better
         // unordered_map<char, int> mpp;
         // int left = 0;
         // int ans = 0;
@@ -27,5 +28,25 @@ public:
         //     ans = max(ans, right - left + 1);
         // }
         // return ans;
+
+
+        // OPRIMAL
+        vector<int>hash(256,-1);
+        int len;
+        int maxlen=0;
+        int n=s.size();
+        int left=0,right=0;
+        while(right<n){
+            if(hash[s[right]]!=-1){
+                if(hash[s[right]]>=left){
+                    left=hash[s[right]]+1;
+                }
+            }
+            len=right-left+1;
+            maxlen=max(maxlen,len);
+            hash[s[right]]=right;
+            right++;
+        }
+        return maxlen;
     }
 };
