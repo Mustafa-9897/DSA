b5fcf253802b166cb4e729be6df723aceac1864d
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-18 22:25:49 +0530

    Time: 11 ms (42.98%) | Memory: 11 MB (80.92%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -35,18 +35,30 @@ public:
     
 
     // CLEAN VERSION OF ABOVE CODE
-    int n = s.size();
-    vector<int> freq(3, 0);
-    int left = 0;
-    int ans = 0;
+    // int n = s.size();
+    // vector<int> freq(3, 0);
+    // int left = 0;
+    // int ans = 0;
 
-    for (int right = 0; right < n; right++) {
-        freq[s[right] - 'a']++;
-        while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-            ans += n - right;
-            freq[s[left] - 'a']--;
-            left++;
-        }
+    // for (int right = 0; right < n; right++) {
+    //     freq[s[right] - 'a']++;
+    //     while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+    //         ans += n - right;
+    //         freq[s[left] - 'a']--;
+    //         left++;
+    //     }
+    // }
+    // return ans;
+
+    //  OPTIMAL
+    int ans=0,a=-1,b=-1,c=-1;
+    int mini=INT_MAX;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='a') a=i;
+        else if(s[i]=='b') b=i;
+        else c=i;
+        mini=min({a,b,c});
+        ans += mini+1;
     }
     return ans;
     }
