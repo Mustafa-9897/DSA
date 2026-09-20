cfa12204e4d19b0551e6cbad36019fa12ff9a3ad
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-20 13:58:48 +0530

    Time: 180 ms (25.34%) | Memory: 100 MB (30.21%) - LeetSync

---
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int l=0,r=nums.size()-1;
+        int maxi=INT_MIN;
+        while(l<r){
+            maxi=max(maxi,nums[l]+nums[r]);
+            l++;
+            r--;
+        }
+        return maxi;
+    }
+};
