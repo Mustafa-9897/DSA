a338b33a1e098a0b7e5af79488122b3e3ba63e40
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-20 23:45:17 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (44.57%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        //sort(bills.begin(),bills.end());  // ypu cannot sort this one becuase saare customers queue me khade hai so by sorting you are changing the queue which we cannot do
+        //sort(bills.begin(),bills.end());  // you cannot sort this one becuase saare customers queue me khade hai so by sorting you are changing the queue which we cannot do
         int b5=0,b10=0,b20=0;
         for(int i=0;i<bills.size();i++){
             if(bills[i]==5) b5++;
