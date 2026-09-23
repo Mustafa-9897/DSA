f959ad7371ebde0897cb8deec767b6f4ac8f5999
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-23 22:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (13.76%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -7,12 +7,12 @@ public:
         for(int i=0;i<n;i++){
             int sum=0;
             if(k>0){
-                for(int j=1;j<=k;j++){
+                for(int j=1;j<=k;j++){   // for k>0 move forward
                     sum += code[(i+j)%n];
                 }
             }
             else{
-                for(int j=1;j<=-k;j++){
+                for(int j=1;j<=-k;j++){   // for k<0 move backwards
                     sum += code[(i-j+n)%n];
                 }
             }
