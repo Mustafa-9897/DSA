46d417b19b0018ccef5e925a895dc6c9ff45d021
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-24 10:51:10 +0530

    Time: 2 ms (90.02%) | Memory: 8.7 MB (59.25%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int countSubstrings(string s) {
+        //    BRUTE
+        // int ans=0;
+        // for(int i=0;i<s.size();i++){
+        //     string t="";
+        //     for(int j=i;j<s.size();j++){
+        //         t += s[j];
+        //         int l=0,r=t.size()-1;
+        //         int flag=1;
+        //         while(l<r){
+        //             if(t[l]!=t[r]){
+        //                 flag=0;
+        //             }
+        //             l++;
+        //             r--;
+        //         }
+        //         if(flag==1) ans++;
+        //     }
+        // }
+        // return ans;
+
+        // OPTIMAL
+        int n=s.size();
         int ans=0;
-        for(int i=0;i<s.size();i++){
-            string t="";
-            for(int j=i;j<s.size();j++){
-                t += s[j];
-                int l=0,r=t.size()-1;
-                int flag=1;
-                while(l<r){
-                    if(t[l]!=t[r]){
-                        flag=0;
-                    }
-                    l++;
-                    r--;
-                }
-                if(flag==1) ans++;
+        for(int i=0;i<n;i++){
+            // odd length string
+            int l=i,r=i;
+            while(l>=0 && r<n &&s[l]==s[r]){
+                ans++;
+                l--;r++;
+            }
+            // even length string
+            l=i,r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                ans++;
+                l--;r++;
             }
         }
         return ans;
