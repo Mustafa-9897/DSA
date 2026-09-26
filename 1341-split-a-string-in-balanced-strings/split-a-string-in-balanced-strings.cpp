072a8f3dfa66690ae0ae0457ac5dd29ca5452104
Mustafa-9897 <mustafababude02@gmail.com>
2026-09-26 22:45:23 +0530

    Time: 2 ms (3.65%) | Memory: 8.2 MB (85.39%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int ans=0;
+        int balance=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='R') balance++;
+            else balance--;
+            if(balance==0) ans++;
+        }
+        return ans;
+    }
+};
